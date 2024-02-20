ce0dbe2288ba7196a97d19e90044dc1b5149b234
allym0806 <128709807+allym0806@users.noreply.github.com>
2024-02-20 21:39:17 +0000

    some updates to streamlit during mentor meeting

--- notebooks/streamlit_visualization.py
+++ notebooks/streamlit_visualization.py
@@ -18,7 +18,7 @@ items['item_id'] = items['item_id'].astype(str)
 df_merged = pd.merge(observations, items, left_on='item_ID', right_on='item_id', how='inner')
 
 with st.sidebar:
-    st.title('Disintegration Dashboard')
+    st.title('Residual Dashboard')
 
     trial_list = ['All trials'] + list(df_merged.trial_ID.unique())
     selected_trials = st.multiselect('Select trial(s)', ['All trials'] + trial_list, default='All trials')
@@ -30,11 +30,14 @@ with st.sidebar:
 
     residual_type = st.selectbox('Choose Residual Type', ['Residual by mass', 'Residual by surface area'])
     residual = 'mass_resid_%' if residual_type == 'Residual by mass' else 'sa_resid_%'
-    material_type = st.selectbox('Choose Material Class', ['Class I', 'Class II', 'Class III'])
+    material_type = st.selectbox('Choose Item Group', ['Class I', 'Class II', 'Class III', 'Item'])
+    # make this a dictionary
     if material_type == 'Class I':
         material = 'material_class_i'
     elif material_type == 'Class II':
         material = 'material_class_ii'
+    elif material_type == 'Item':
+        material = 'item_name'
     else:  
         material = 'material_class_iii'
     cap_anomalies = st.checkbox('Without Anomalies')
@@ -42,7 +45,7 @@ with st.sidebar:
 def bar_whisker_plot(df, x, y, cap_anomalies):
 
     if cap_anomalies:
-        df = df[df[y] <= 100]
+        df[y] = df[y].clip(lower=0, upper=100)
 
     iqr = df.groupby(x)[y].apply(lambda g: g.quantile(0.75) - g.quantile(0.25))
     nonzero_iqr_order = df[df[x].isin(iqr[iqr > 0].index)].groupby(x)[y].median().sort_values(ascending=False).index
@@ -58,10 +61,7 @@ def bar_whisker_plot(df, x, y, cap_anomalies):
     plt.tight_layout()
     plt.show()
 
-st.markdown('#### Disintegration Rate Analysis by Items')
-item_residual = bar_whisker_plot(df_selected_trial, 'item_name', residual, cap_anomalies)
+st.markdown('#### Product Residual Analysis')
+item_residual = bar_whisker_plot(df_selected_trial, material, residual, cap_anomalies)
 st.pyplot(plt)
 
-st.markdown('#### Disintegration Rate Analysis by Material Class')
-material_residual = bar_whisker_plot(df_selected_trial, material, residual, cap_anomalies)
-st.pyplot(plt)
