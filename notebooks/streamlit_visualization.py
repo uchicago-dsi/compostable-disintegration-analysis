134e356d17dafcb549c773d06684159502e50ec1
ceciliazhang0329 <128743117+ceciliazhang0329@users.noreply.github.com>
2024-02-23 15:42:19 -0600

    test the work directory for streamlit app

--- notebooks/streamlit_visualization.py
+++ notebooks/streamlit_visualization.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+
+st.write("Current Working Directory:", os.getcwd())
 
 st.set_page_config(
     page_title="Disintegration Dashboard",
