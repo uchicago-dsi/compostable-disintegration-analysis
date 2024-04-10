e77ae5abce32ad15777be9b2183cafeae77f4f9f
toddnief <todd.nief@gmail.com>
2024-04-10 19:51:48 +0000

    casp004 works - wr003 doesn't

--- notebooks/pipeline-template.py
+++ notebooks/pipeline-template.py
@@ -27,19 +27,27 @@ TRIAL_COLS = [
 
 
 class AbstractDataPipeline(ABC):
-    def __init__(self, data_filepath, items_filepath=ITEMS_PATH, trial=None, sheet_name=0, skiprows=0):
+    def __init__(
+        self,
+        data_filepath,
+        items_filepath=ITEMS_PATH,
+        trial=None,
+        sheet_name=0,
+        skiprows=0,
+    ):
         self.data_filepath = data_filepath
         filename, _ = self.data_filepath.rsplit(".", 1)
         file_suffix = f"_{trial}_clean.csv" if trial else "_clean.csv"
         self.output_filepath = filename + file_suffix
 
         # TODO: This is kind of messy and could probably be better
-        self.data = self.load_data(data_filepath, sheet_name=sheet_name, skiprows=skiprows)
+        self.data = self.load_data(
+            data_filepath, sheet_name=sheet_name, skiprows=skiprows
+        )
         self.items = self.load_items(items_filepath)
         # TODO: Comment the below lines to try CASP004
         self.item2id = self.load_items_map()
         self.item2id = self.load_extra_items_map() | self.item2id
-
 
     @abstractmethod
     def load_data(self, data_filepath, sheet_name=0, skip_rows=0):
@@ -62,16 +70,15 @@ class AbstractDataPipeline(ABC):
             .to_dict()
             .items()
         }
-    
+
     def load_extra_items_map(self):
         extra_items = pd.read_excel(EXTRA_ITEMS_PATH)
-        return extra_items.set_index("OG Description")['Item ID'].to_dict()
+        return extra_items.set_index("OG Description")["Item ID"].to_dict()
 
     def preprocess_data(self, df):
         return df
 
     def join_with_items(self, df):
-        """Processes the weight and area DataFrames"""
         return pd.merge(self.items, df, on="Item ID")
 
     def process_data(self, df):
@@ -92,35 +99,20 @@ class AbstractDataPipeline(ABC):
         if save:
             df.to_csv(self.output_filepath, index=False)
         return df
-    
+
 
 class CASP004Pipeline(AbstractDataPipeline):
-    def load_items(self, items_filepath):
-        """Loads the items DataFrame."""
-        return pd.read_excel(items_filepath, sheet_name=2).drop_duplicates(
-            subset=["Item Name"]
-        )
-
-    # def load_items_map(self):
-    #     return {
-    #         key.strip(): value
-    #         for key, value in self.items.set_index("Item Description")[
-    #             "Item ID"
-    #         ]
-    #         .to_dict()
-    #         .items()
-    #     }
 
     def load_data(self, data_filepath, sheet_name=0, skiprows=0):
         return pd.read_excel(data_filepath, sheet_name=sheet_name, skiprows=skiprows)
-    
-    # TODO: Ok...not sure how to set this up for this trial actually
-    # items_casp004.set_index('Item Name')['Weight (average)'].to_dict()
-    # observations_casp004['Start Weight'] = observations_casp004['Product Name'].map(casp004_weights)
 
-    def join_with_items(self, df):
-        """Processes the weight and area DataFrames"""
-        return pd.merge(self.items, df, left_on="Item Name", right_on="Product Name")
+    def load_items(self, items_filepath):
+        items = super().load_items(items_filepath)
+
+        # We are using the start weight specific to this trial so drop the Start Weight column
+        # Start weight is set in preprocess_data
+        items = items.drop("Start Weight", axis=1)
+        return items
 
     def preprocess_data(self, data):
         # Only use observations at the end
@@ -128,21 +120,49 @@ class CASP004Pipeline(AbstractDataPipeline):
         # Bags A-5 and A-6 were not found
         df = df[~df["Bag Id"].isin(["A-5", "A-6"])]
 
+        df["Trial"] = df["Trial Id"]
+
         # Take the average of the three weight observations
         df["End Weight"] = df[["Weight 1", "Weight 2", "Weight 3"]].mean(axis=1)
 
         # Null values mean the item fully disintegrated
         df["End Weight"] = df["End Weight"].fillna(0)
 
+        # Ok...we need to do some weird items work arounds here...this might work?
+        casp004_items = pd.read_excel(self.data_filepath, sheet_name=2).drop_duplicates(
+            subset=["Item Name"]
+        )
+        casp004_weights = casp004_items.set_index("Item Name")[
+            "Weight (average)"
+        ].to_dict()
+        df["Start Weight"] = df["Product Name"].map(casp004_weights)
+        # rename so this matches the other trials
+        df["Item Description Refined"] = df["Product Name"]
+
+        # TODO: Do we want to merge on ID or should we just merge on description if we have it?
+        df["Item ID"] = df["Item Description Refined"].str.strip().map(self.item2id)
+        # TODO: Make this more generalizable
+        # Prevent duplicate columns when merging with items
+        drop_cols = ["Item Description Refined"]
+        df = df.drop(drop_cols, axis=1)
+        assert df["Item ID"].isnull().sum() == 0, "There are null items after mapping"
+
+        return df
+
+    def process_data(self, df):
+        df["End Weight"] = df[["Weight 1", "Weight 2", "Weight 3"]].mean(axis=1)
+        df["End Weight"] = df["End Weight"].fillna(0)
+
+        df["% Residuals (Area)"] = None
+        df["% Residuals (Weight)"] = df["End Weight"] / df["Start Weight"]
         return df
 
 
 CASP004_PATH = (
     DATA_FOLDER + "CASP004-01 - Results Pre-Processed for Analysis from PDF Tables.xlsx"
 )
-# casp004_pipeline = CASP004Pipeline(CASP004_PATH, items_filepath=CASP004_PATH, sheet_name=1)
-# TODO: This breaks
-# casp004_processed = casp004_pipeline.run(save=True)
+casp004_pipeline = CASP004Pipeline(CASP004_PATH, sheet_name=1)
+casp004_processed = casp004_pipeline.run(save=True)
 
 
 class ClosedLoopPipeline(AbstractDataPipeline):
@@ -195,9 +215,9 @@ class ClosedLoopPipeline(AbstractDataPipeline):
         )
 
     def preprocess_data(self, df):
-        df_pp = df[df["Trial Stage"] == "Second Removal"]
-        df_pp = df_pp.rename(columns={"Facility Name": "Trial"})
-        return df_pp
+        df = df[df["Trial Stage"] == "Second Removal"]
+        df = df.rename(columns={"Facility Name": "Trial"})
+        return df
 
 
 TEN_TRIALS_PATH = (
@@ -205,6 +225,7 @@ TEN_TRIALS_PATH = (
 )
 closed_loop_pipeline = ClosedLoopPipeline(TEN_TRIALS_PATH)
 closed_loop_processed = closed_loop_pipeline.run(save=True)
+
 
 class PDFPipeline(AbstractDataPipeline):
     def __init__(self, *args, weight_col="Residual Weight - Oven-dry", **kwargs):
@@ -214,15 +235,15 @@ class PDFPipeline(AbstractDataPipeline):
     def load_data(self, data_filepath, sheet_name=0, skiprows=0):
         return pd.read_excel(data_filepath, sheet_name=sheet_name, skiprows=skiprows)
 
-    # TODO: Maybe need to add setup for extra items here
-    def join_with_items(self, df):
+    def preprocess_data(self, df):
         # TODO: Do we want to merge on ID or should we just merge on description if we have it?
         df["Item ID"] = df["Item Description Refined"].str.strip().map(self.item2id)
+        # TODO: Make this more generalizable
         # Prevent duplicate columns when merging with items
-        drop_cols = ["Item Description From Trial", "Item Description Refined"]
+        drop_cols = ["Item Description Refined", "Item Description From Trial"]
         df = df.drop(drop_cols, axis=1)
         assert df["Item ID"].isnull().sum() == 0, "There are null items after mapping"
-        return pd.merge(self.items, df, on="Item ID")
+        return df
 
     def process_data(self, df):
         df["% Residuals (Weight)"] = df[self.weight_col] / (
@@ -248,13 +269,22 @@ casp001_processed = casp001_pipeline.run(save=True)
 
 # TODO: This has null items — figure out the extra items setup
 
+
 class CASP003Pipeline(PDFPipeline):
     def preprocess_data(self, data):
         # everything in blug bags was combined and impossible to separate
-        return data[data['Trial Bag Colour'] != "Blue"]
+        return data[data["Trial Bag Colour"] != "Blue"]
 
-casp003_pipeline = CASP003Pipeline(PDF_TRIALS, trial="casp003", sheet_name=3, weight_col="Final Residual Weight - wet - aggregate")
+
+casp003_pipeline = CASP003Pipeline(
+    PDF_TRIALS,
+    trial="casp003",
+    sheet_name=3,
+    weight_col="Final Residual Weight - wet - aggregate",
+)
 casp003_processed = casp003_pipeline.run(save=True)
 
-wr003_pipeline = PDFPipeline(PDF_TRIALS, trial="wr003", sheet_name=4, weight_col="Final Residual Weight - wet")
+wr003_pipeline = PDFPipeline(
+    PDF_TRIALS, trial="wr003", sheet_name=4, weight_col="Final Residual Weight - wet"
+)
 wr003_processed = wr003_pipeline.run(save=True)
