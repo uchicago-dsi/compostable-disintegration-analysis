4a0b3a88ac08e68ed6de0e7932ea1a788b9d2722
toddnief <todd.nief@gmail.com>
2024-04-12 20:53:39 +0000

    streamlit works with plotly

--- dashboard/streamlit_visualization.py
+++ dashboard/streamlit_visualization.py
@@ -14,8 +14,8 @@ st.set_page_config(
 
 # Assuming the CSV files are in the correct directories and accessible
 df = pd.read_csv("dashboard/all_trials_processed.csv")
-df["% Residuals (Weight)"] = df["% Residuals (Weight)"] * 100
-df["% Residuals (Area)"] = df["% Residuals (Area)"] * 100
+# df["% Residuals (Weight)"] = df["% Residuals (Weight)"] * 100
+# df["% Residuals (Area)"] = df["% Residuals (Area)"] * 100
 # observations = observations.astype({"item_ID": str, "facility_ID": str})
 # items = pd.read_csv("dashboard/data/items.csv")
 # items["item_id"] = items["item_id"].astype(str)
@@ -150,6 +150,132 @@ def bar_whisker_plot(df, x, y, cap_anomalies):
     plt.show()
 
 
+import matplotlib.colors as mcolors
+import numpy as np
+import plotly.graph_objects as go
+
+class2color = {
+    "Positive Control": "#70AD47",
+    "Mixed Materials": "#48646A",
+    "Fiber": "#298FC2",
+    "Biopolymer": "#FFB600",
+}
+
+class_I_order = ["Fiber", "Biopolymer", "Mixed Materials", "Positive Control"]
+class_II_order = [
+    "Unlined Fiber",
+    "Lined Fiber",
+    "Biopolymer Film/Bag",
+    "Rigid Biopolymer (> 0.75mm)",
+    "Rigid Biopolymer (< 0.75mm)",
+    "Positive Control - Fiber",
+    "Positive Control - Film",
+    "Positive Control - Food Scraps",
+]
+
+
+def box_and_whisker(
+    df_input, column, class_I=None, cap=False, height=800, width=1000, save=False
+):
+    df = df_input.copy()  # prevent modifying actual dataframe
+
+    data = []
+    x_labels = []
+
+    if cap:
+        df[column] = df[column].clip(upper=1)
+    if class_I:
+        df = df[df["Material Class I"] == class_I]
+
+    max_value = df[column].max()
+    max_value = max(100, max_value)
+
+    for class_II in class_II_order:
+        group = df[df["Material Class II"] == class_II]
+        if not group.empty:
+            count = group[column].count()
+            class_I_name = group["Material Class I"].iloc[0]
+            color = class2color.get(class_I_name, "#000")
+            trace = go.Box(
+                y=group[column],
+                name=class_II,
+                boxpoints="outliers",
+                marker_color=color,
+                width=0.3,
+            )
+            data.append(trace)
+            x_labels.append(f"     {class_II}<br>     n={count}")
+
+    y_axis_title = f"{column}"
+    if cap:
+        y_axis_title += " Capped"
+
+    layout = go.Layout(
+        title_font=dict(size=14, family="Roboto"),
+        font=dict(family="Roboto", size=11),
+        height=height,
+        width=width,
+        showlegend=False,
+        xaxis=dict(
+            tickmode="array",
+            tickvals=list(range(len(x_labels))),
+            ticktext=x_labels,
+            title_font=dict(size=14),
+            tickfont=dict(size=11),
+            tickangle=90,
+        ),
+        yaxis=dict(
+            title=y_axis_title,
+            tickformat=".0%",
+            tickmode="array",
+            tickvals=np.arange(
+                0, max_value, 0.25
+            ),  # Adjust this range if your data is not percentage-based
+            title_font=dict(size=16),
+            tickfont=dict(size=9),
+            rangemode="tozero",
+        ),
+    )
+
+    layout = go.Layout(
+        height=height,
+        width=width,
+        showlegend=False,
+        xaxis=dict(
+            tickmode="array",
+            tickvals=list(range(len(x_labels))),
+            ticktext=x_labels,
+            title_font=dict(size=14),
+            tickfont=dict(size=11),
+            tickangle=90,
+        ),
+        yaxis=dict(
+            title=y_axis_title,
+            tickformat=".0%",
+            tickmode="array",
+            tickvals=np.arange(
+                0, max_value, 0.25
+            ),  # Adjust this range if your data is not percentage-based
+            title_font=dict(size=16),
+            tickfont=dict(size=9),
+            rangemode="tozero",
+        ),
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    if save:
+        filepath = column.replace(" ", "_") + "_box_and_whisker"
+
+        if cap:
+            filepath += "_capped"
+
+        filepath += ".png"
+        fig.write_image(filepath)
+
+    return fig
+
+
 st.markdown("#### Product Residual Analysis of Field Testing Results")
 st.write(
     """
@@ -161,5 +287,10 @@ st.write(
 
     """
 )
-item_residual = bar_whisker_plot(df_selected_tech, material, residual, cap_anomalies)
-st.pyplot(plt)
+# item_residual = bar_whisker_plot(df_selected_tech, material, residual, cap_anomalies)
+# st.pyplot(plt)
+
+fig = box_and_whisker(
+    df, column=residual, class_I=None, cap=cap_anomalies, height=800, width=1000
+)
+st.plotly_chart(fig, use_container_width=True)
