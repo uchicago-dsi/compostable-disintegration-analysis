d8d26c82e2add434242692b2e33f58f619cb2b3a
toddnief <todd.nief@gmail.com>
2024-06-27 18:38:48 -0500

    half-baked dash is not breaking

--- dashboard-react/app/api/data/route.js
+++ dashboard-react/app/api/data/route.js
@@ -5,6 +5,15 @@ import fs from 'fs/promises';
 
 const dataPath = path.join(process.cwd(), 'public', 'data', 'all_trials_processed.csv');
 
+const moistureDict = {
+  "<40%": [-Infinity, 0.4, false],
+  "40-45%": [0.4, 0.45, true],
+  "45-50%": [0.45, 0.50, true],
+  "50-55%": [0.50, 0.55, true],
+  "55-60%": [0.55, 0.60, true],
+  ">60%": [0.60, Infinity, false],
+};
+
 const fetchData = async () => {
   const data = await fs.readFile(dataPath, 'utf8');
   return d3.csvParse(data);
@@ -21,9 +30,52 @@ const calculateQuartiles = (data, key) => {
   };
 };
 
-const prepareData = async (aggCol, displayCol) => {
+const getFilteredTrialIDs = (data, col, low, high, inclusive) => {
+return data.filter(d => {
+  const value = parseFloat(d[col]);
+  if (inclusive) {
+    return value >= low && value <= high;
+  }
+  return value > low && value < high;
+}).map(d => d["Trial ID"]);
+};
+
+const applyFilters = (data, filters) => {
+  console.log(filters)
+  return data.filter(d => {
+    return Object.entries(filters).every(([col, filterValue]) => {
+      // if (!filterValue || filterValue === 'All') {
+      //   return true; // No filter applied for this column
+      // }
+      if (Array.isArray(filterValue)) {
+        return filterValue.some(val => d[col] === val);
+      }
+      return d[col] === filterValue;
+    });
+  });
+};
+
+const prepareData = async (searchParams) => {
   const data = await fetchData();
-  const grouped = d3.groups(data, d => d[aggCol]);
+
+  const aggCol = searchParams.get('aggcol') || 'Material Class I';
+  const displayCol = searchParams.get('displaycol') || '% Residuals (Mass)';
+  // TODO: Can I split this out somehow and iterate through?
+  const testMethods = searchParams.get('testmethods') || '';
+  const moistureFilter = searchParams.get('moisture') || 'All Moistures';
+
+  // Filter data on params
+  // TODO: make this generalizable
+  const filteredData = testMethods.length > 0
+    ? data.filter(d => testMethods.includes(d['Test Method']))
+    : data;
+
+  // const brokenFilteredData = applyFilters(data, {
+  //   'Test Method': selectedTestMethods,
+  // });
+  // console.log(brokenFilteredData)
+
+  const grouped = d3.groups(filteredData, d => d[aggCol]);
   return grouped.map(([key, values]) => ({
     aggCol: key,
     ...calculateQuartiles(values, displayCol),
@@ -32,9 +84,6 @@ const prepareData = async (aggCol, displayCol) => {
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
-  const aggCol = searchParams.get('aggcol') || 'Material Class I';
-  const displayCol = searchParams.get('displaycol') || '% Residuals (Mass)';
-  console.log(displayCol)
-  const data = await prepareData(aggCol, displayCol);
+  const data = await prepareData(searchParams);
   return NextResponse.json(data);
 }
--- dashboard-react/app/page.js
+++ dashboard-react/app/page.js
@@ -12,8 +12,9 @@ const Home = () => {
       console.log("Fetching data...");
       let url = new URL("/api/data", window.location.origin);
       let params = new URLSearchParams(url.search);
-      params.append("aggcol", snap.aggCol);
-      params.append("displaycol", snap.displayCol);
+      params.append("aggcol", snap.filters.aggCol);
+      params.append("displaycol", snap.filters.displayCol);
+      params.append("testmethods", snap.filters.selectedTestMethods.join(","));
       url.search = params.toString();
       console.log(params.toString());
 
@@ -23,16 +24,38 @@ const Home = () => {
       state.setData(result);
     };
     fetchData();
-  }, [snap]);
+  }, [snap.filters]);
+
+  useEffect(() => {
+    const fetchOptions = async () => {
+      console.log("Fetching options...");
+      const response = await fetch("/api/options");
+      const result = await response.json();
+      console.log(result);
+      Object.keys(result).forEach(key => {
+        state.setOptions(key, result[key]);
+      });
+    };
+    fetchOptions();
+  }, []);
 
   const handleSelectionChange = (key) => (event) => {
     console.log(key, event.target.value);
-    state.setStateValue(key, event.target.value);
+    state.setFilterValue(key, event.target.value);
+  };
+
+  const handleCheckboxChange = (key, value) => (event) => {
+    const checked = event.target.checked;
+    if (checked) {
+      state.setFilterValue(key, [...snap.filters[key], value]);
+    } else {
+      state.setFilterValue(key, snap.filters[key].filter(item => item !== value));
+    }
   };
 
   const plotData = snap.data.map(d => ({
     type: 'box',
-    name: d[snap.aggCol],
+    name: d[snap.filters.aggCol],
     y: [d.min, d.q1, d.median, d.q3, d.max]
   }));
 
@@ -41,16 +64,29 @@ const Home = () => {
       <h1>CFTP Field Test Results Dashboard</h1>
       <div>
         <label htmlFor="columnSelect">Select Column:</label>
-        <select id="columnSelect" value={snap.aggCol} onChange={handleSelectionChange('aggCol')}>
+        <select id="columnSelect" value={snap.filters.aggCol} onChange={handleSelectionChange('aggCol')}>
           <option value="Material Class I">Material Class I</option>
           <option value="Material Class II">Material Class II</option>
           <option value="Material Class III">Material Class III</option>
         </select>
-        <label htmlFor="quartilesColumnSelect">Select Quartiles Column:</label>
-        <select id="quartilesColumnSelect" value={snap.displayCol} onChange={handleSelectionChange('displayCol')}>
+        <label htmlFor="displayColumnSelect">Select Display Column:</label>
+        <select id="displayColumnSelect" value={snap.filters.displayCol} onChange={handleSelectionChange('displayCol')}>
           <option value="% Residuals (Mass)">% Residuals (Mass)</option>
           <option value="% Residuals (Area)">% Residuals (Area)</option>
         </select>
+        <label>Select Test Methods:</label>
+          {snap.options["Test Method"]?.map(value => (
+            <div key={value}>
+              <input
+                type="checkbox"
+                id={`testMethod-${value}`}
+                value={value}
+                checked={snap.filters.selectedTestMethods.includes(value)}
+                onChange={handleCheckboxChange('selectedTestMethods', value)}
+              />
+              <label htmlFor={`testMethod-${value}`}>{value}</label>
+            </div>
+          ))}
       </div>
       <Plot
         data={plotData}
