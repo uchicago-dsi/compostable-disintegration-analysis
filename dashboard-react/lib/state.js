d750bf54b47563352f95fc6201838d7b2746e6f0
toddnief <todd.nief@gmail.com>
2024-06-28 17:32:56 -0500

    remove lib from .gitignore

---
+++ dashboard-react/lib/state.js
@@ -0,0 +1,29 @@
+"use client";
+import { proxy } from "valtio";
+
+const state = proxy({
+  data: [],
+
+  setData: (newData) => {
+    state.data = newData;
+  },
+
+  options: {},
+
+  setOptions: (key, newOptions) => {
+    state.options[key] = newOptions;
+  },
+
+  filters: {
+    aggCol: 'Material Class I',
+    displayCol: '% Residuals (Mass)',
+    selectedTechnologies: [],
+    selectedTestMethods: [],
+  },
+
+  setFilterValue: (key, value) => {
+    state.filters[key] = value;
+  },
+});
+
+export default state;
