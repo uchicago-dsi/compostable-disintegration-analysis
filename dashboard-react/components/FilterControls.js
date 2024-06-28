c1450658da366e2d456487a2227f85594baaf174
toddnief <todd.nief@gmail.com>
2024-06-28 15:27:53 -0500

    keep tweaking sidebar layout

--- dashboard-react/components/FilterControls.js
+++ dashboard-react/components/FilterControls.js
@@ -19,39 +19,60 @@ const SelectionControls = () => {
     }
   };
 
+    //   {/* Show by Residuals Remaining or by Percent Disintegrated */}
+    // {/* Cap results */}
+
+      
+    // {/* Select material type(s) */}
+    // {/* Select technology */}
+
+    // {/* Operating Conditions Filters */}
+    // {/* Select Average Temperature Range */}
+    // {/* Select Trial Duration Range */}
+    // {/* Select Average Moisture Content (In Field) Range */}
+
   return (
+    <>
     <div>
-      <div>
-        <label htmlFor="columnSelect">Select Column:</label>
-        <select id="columnSelect" value={snap.filters.aggCol} onChange={handleSelectionChange('aggCol')}>
-            <option value="Material Class I">Material Class I</option>
-            <option value="Material Class II">Material Class II</option>
-            <option value="Material Class III">Material Class III</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="displayColumnSelect">Select Display Column:</label>
-        <select id="displayColumnSelect" value={snap.filters.displayCol} onChange={handleSelectionChange('displayCol')}>
-            <option value="% Residuals (Mass)">% Residuals (Mass)</option>
-            <option value="% Residuals (Area)">% Residuals (Area)</option>
-        </select>
-      </div>
-      <div>
-      <label>Select Test Methods:</label>
-      {snap.options["Test Method"]?.map(value => (
-        <div key={value}>
-          <input
-            type="checkbox"
-            id={`testMethod-${value}`}
-            value={value}
-            checked={snap.filters.selectedTestMethods.includes(value)}
-            onChange={handleCheckboxChange('selectedTestMethods', value)}
-          />
-          <label htmlFor={`testMethod-${value}`}>{value}</label>
+        <div>
+            <label htmlFor="columnSelect">Select Column:</label>
+            <select id="columnSelect" value={snap.filters.aggCol} onChange={handleSelectionChange('aggCol')}>
+                <option value="Material Class I">Material Class I</option>
+                <option value="Material Class II">Material Class II</option>
+                <option value="Material Class III">Material Class III</option>
+            </select>
         </div>
-      ))}
-      </div>
-    </div>
+        <div>
+            <label htmlFor="displayColumnSelect">Choose x-axis Display:</label>
+            <select id="displayColumnSelect" value={snap.filters.displayCol} onChange={handleSelectionChange('displayCol')}>
+                <option value="% Residuals (Mass)">% Residuals (Mass)</option>
+                <option value="% Residuals (Area)">% Residuals (Area)</option>
+            </select>
+        </div>
+        <div>
+        <label>Select Test Methods:</label>
+        {snap.options["Test Method"]?.map(value => (
+            <div key={value}>
+            <input
+                type="checkbox"
+                id={`testMethod-${value}`}
+                value={value}
+                checked={snap.filters.selectedTestMethods.includes(value)}
+                onChange={handleCheckboxChange('selectedTestMethods', value)}
+            />
+            <label htmlFor={`testMethod-${value}`}>{value}</label>
+            </div>
+        ))}
+        </div>
+        <div>
+            <p>
+                <i>
+                    Note: There are some results by both mass or surface area with over 100% residuals. The dashboard automatically caps these results at 100% residuals (0% disintegration). Check this box to show all results, including over 100% Residuals. Disintegration results are always capped at 0% (no negative disintegration results)
+                </i>
+            </p>
+        </div>
+        </div>
+    </>
   );
 };
 
