560f113c3f6b5aba4348aa06a4cbd202d50fba3e
toddnief <todd.nief@gmail.com>
2024-06-29 11:23:43 -0500

    operating conditions filter is generalizable

--- dashboard-react/components/FilterControls.js
+++ dashboard-react/components/FilterControls.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSnapshot } from 'valtio';
 import state from '@/lib/state';
-import { moistureDict } from '@/lib/constants';
+import { moistureFilterDict, temperatureFilterDict } from '@/lib/constants';
 
 const FilterControls = () => {
   const snap = useSnapshot(state);
@@ -31,68 +31,82 @@ const FilterControls = () => {
 
   return (
     <>
-    <div>
-        <h2>Display Options</h2>
         <div>
-            <label htmlFor="columnSelect">Select Column:</label>
-            <select id="columnSelect" value={snap.filters.aggCol} onChange={handleSelectionChange('aggCol')}>
-                <option value="Material Class I">Material Class I</option>
-                <option value="Material Class II">Material Class II</option>
-                <option value="Material Class III">Material Class III</option>
-            </select>
-        </div>
-        <div>
-            <label htmlFor="displayColumnSelect">Choose x-axis Display:</label>
-            <select id="displayColumnSelect" value={snap.filters.displayCol} onChange={handleSelectionChange('displayCol')}>
-                <option value="% Residuals (Mass)">% Residuals (Mass)</option>
-                <option value="% Residuals (Area)">% Residuals (Area)</option>
-            </select>
-        </div>
-        <div>
-        <label htmlFor="capResults">Display All Results:</label>
-            <input
-                type="checkbox"
-                id="capResults"
-                checked={snap.filters.uncapResults}
-                onChange={() => state.setFilterValue('uncapResults', !snap.filters.uncapResults)}
-            />
-            <p>
-                <i>
-                    Note: There are some results by both mass or surface area with over 100% residuals. The dashboard automatically caps these results at 100% residuals (0% disintegration). Check this box to show all results, including over 100% Residuals. Disintegration results are always capped at 0% (no negative disintegration results)
-                </i>
-            </p>
-        </div>
-        <h2>Filters</h2>
-        <div>
-        <label>Select Test Methods:</label>
-        {snap.options["Test Method"]?.map(value => (
-            <div key={value}>
-            <input
-                type="checkbox"
-                id={`testMethod-${value}`}
-                value={value}
-                checked={snap.filters.selectedTestMethods.includes(value)}
-                onChange={handleCheckboxChange('selectedTestMethods', value)}
-            />
-            <label htmlFor={`testMethod-${value}`}>{value}</label>
+            <h2>Display Options</h2>
+            <div>
+                <label htmlFor="columnSelect">Select Column:</label>
+                <select id="columnSelect" value={snap.filters.aggCol} onChange={handleSelectionChange('aggCol')}>
+                    <option value="Material Class I">Material Class I</option>
+                    <option value="Material Class II">Material Class II</option>
+                    <option value="Material Class III">Material Class III</option>
+                </select>
             </div>
-        ))}
-        </div>
-        <h2>Operating Conditions Filters</h2>
-        <div>
-        <label>Select Average Moisture Content (In Field) Range:</label>
-        {Object.entries(moistureDict)?.map(([key, value]) => (
-            <div key={key}>
-            <input
-                type="checkbox"
-                id={`moisture-${key}`}
-                value={key}
-                onChange={handleCheckboxChange('selectedMoistureLevels', key)}
-            />
-            <label htmlFor={`testMethod-${key}`}>{key}</label>
+            <div>
+                <label htmlFor="displayColumnSelect">Choose x-axis Display:</label>
+                <select id="displayColumnSelect" value={snap.filters.displayCol} onChange={handleSelectionChange('displayCol')}>
+                    <option value="% Residuals (Mass)">% Residuals (Mass)</option>
+                    <option value="% Residuals (Area)">% Residuals (Area)</option>
+                </select>
             </div>
-        ))}
-        </div>
+            <div>
+            <label htmlFor="capResults">Display All Results:</label>
+                <input
+                    type="checkbox"
+                    id="capResults"
+                    checked={snap.filters.uncapResults}
+                    onChange={() => state.setFilterValue('uncapResults', !snap.filters.uncapResults)}
+                />
+                <p>
+                    <i>
+                        Note: There are some results by both mass or surface area with over 100% residuals. The dashboard automatically caps these results at 100% residuals (0% disintegration). Check this box to show all results, including over 100% Residuals. Disintegration results are always capped at 0% (no negative disintegration results)
+                    </i>
+                </p>
+            </div>
+            <h2>Filters</h2>
+            <div>
+                <label>Select Test Methods:</label>
+                {snap.options["Test Method"]?.map(value => (
+                    <div key={value}>
+                    <input
+                        type="checkbox"
+                        id={`testMethod-${value}`}
+                        value={value}
+                        checked={snap.filters.selectedTestMethods.includes(value)}
+                        onChange={handleCheckboxChange('selectedTestMethods', value)}
+                    />
+                    <label htmlFor={`testMethod-${value}`}>{value}</label>
+                    </div>
+                ))}
+            </div>
+            <h2>Operating Conditions Filters</h2>
+            <div>
+                <label>Select Average Temperature Range:</label>
+                {Object.entries(temperatureFilterDict)?.map(([key, value]) => (
+                    <div key={key}>
+                    <input
+                        type="checkbox"
+                        id={`moisture-${key}`}
+                        value={key}
+                        onChange={handleCheckboxChange('selectedTemperatureLevels', key)}
+                    />
+                    <label htmlFor={`testMethod-${key}`}>{key}</label>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <label>Select Average Moisture Content (In Field) Range:</label>
+                {Object.entries(moistureFilterDict)?.map(([key, value]) => (
+                    <div key={key}>
+                    <input
+                        type="checkbox"
+                        id={`moisture-${key}`}
+                        value={key}
+                        onChange={handleCheckboxChange('selectedMoistureLevels', key)}
+                    />
+                    <label htmlFor={`testMethod-${key}`}>{key}</label>
+                    </div>
+                ))}
+            </div>
         </div>
     </>
   );
--- dashboard-react/lib/constants.js
+++ dashboard-react/lib/constants.js
@@ -1,4 +1,4 @@
-export const moistureDict = {
+export const moistureFilterDict = {
     "<40%": [-Infinity, 0.4, false],
     "40-45%": [0.4, 0.45, true],
     "45-50%": [0.45, 0.50, true],
@@ -6,3 +6,10 @@ export const moistureDict = {
     "55-60%": [0.55, 0.60, true],
     ">60%": [0.60, Infinity, false],
   };
+
+export const temperatureFilterDict = {
+    "<140F": [-Infinity, 140, false],
+    "140-150F": [140, 150, true],
+    "150-160F": [150, 160, true],
+    ">160F": [160, Infinity, false],
+}
