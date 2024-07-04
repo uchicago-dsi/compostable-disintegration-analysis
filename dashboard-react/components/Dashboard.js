bd9fffa5d33f9f86fc43ef4a97269aa90a245699
toddnief <todd.nief@gmail.com>
2024-07-04 10:16:05 -0500

    plot is now colored and sorted

--- dashboard-react/components/Dashboard.js
+++ dashboard-react/components/Dashboard.js
@@ -7,13 +7,26 @@ import state from "@/lib/state";
 export default function Dashboard() {
   const snap = useSnapshot(state);
 
+  const class2color = {
+    "Positive Control": "#70AD47",
+    "Mixed Materials": "#48646A",
+    Fiber: "#298FC2",
+    Biopolymer: "#FFB600",
+  };
+
   const plotData =
     Object.keys(snap.data).length > 0
-      ? snap.data.map((d) => ({
-          type: "box",
-          name: d["aggCol"],
-          y: [d.min, d.q1, d.median, d.q3, d.max],
-        }))
+      ? snap.data.map((d) => {
+          console.log(d);
+          const materialClass = d["Material Class I"];
+          const color = class2color[materialClass] || "#000";
+          return {
+            type: "box",
+            name: d["aggCol"],
+            y: [d.min, d.q1, d.median, d.q3, d.max],
+            marker: { color },
+          };
+        })
       : [];
 
   return (
