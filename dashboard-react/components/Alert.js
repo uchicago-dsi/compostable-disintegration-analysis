7cf3359a75e4d494eb27f941e80f287d99d20311
toddnief <todd.nief@gmail.com>
2024-07-14 11:53:22 -0500

    add margin to alert component

--- dashboard-react/components/Alert.js
+++ dashboard-react/components/Alert.js
@@ -3,7 +3,7 @@ import React from "react";
 const AlertMessage = ({ message }) => {
   return (
     <div
-      className={`border-l-4 p-4 bg-yellow-100 border-yellow-400 text-yellow-700} rounded`}
+      className={`border-l-4 p-4 bg-yellow-100 border-yellow-400 text-yellow-700 rounded mt-5`}
     >
       <p className="font-bold">{message}</p>
     </div>
