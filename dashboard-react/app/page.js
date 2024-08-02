588bfa87dadfbc5de95a5b667804bb6b99c3ef6d
toddnief <todd.nief@gmail.com>
2024-08-02 14:46:00 -0500

    add more space for menus

--- dashboard-react/app/page.js
+++ dashboard-react/app/page.js
@@ -23,11 +23,11 @@ const Home = () => {
   }, []);
 
   return (
-    <main className="flex flex-col items-start h-[1200px] overflow-hidden">
+    <main className="flex flex-col items-start h-[1250px] overflow-hidden">
       <div className="max-w-[1440px] h-[600px] overflow-hidden mx-auto">
         <Dashboard />
       </div>
-      <div className="max-w-[1440px] h-[600px] mx-auto">
+      <div className="max-w-[1440px] h-[650px] mx-auto">
         <DashboardControls />
       </div>
     </main>
