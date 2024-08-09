ca9233bf70e2c7463e94418c67ec7aa8f521df66
toddnief <todd.nief@gmail.com>
2024-08-09 15:17:19 -0500

    move data fetching to utils

---
+++ dashboard-react/lib/serverUtils.js
@@ -0,0 +1,32 @@
+import * as d3 from "d3";
+import { Storage } from "@google-cloud/storage";
+import fs from "fs/promises";
+
+const serviceAccountKey = JSON.parse(
+  Buffer.from(
+    process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64,
+    "base64"
+  ).toString("ascii")
+);
+
+export const fetchCloudData = async (filename, bucketName) => {
+  const storage = new Storage({
+    credentials: serviceAccountKey,
+  });
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(filename);
+
+  try {
+    const [fileContents] = await file.download();
+    const data = d3.csvParse(fileContents.toString("utf-8"));
+    return data;
+  } catch (error) {
+    console.error("Error downloading file:", error);
+    throw error;
+  }
+};
+
+export const fetchLocalData = async (dataPath) => {
+  const data = await fs.readFile(dataPath, "utf8");
+  return d3.csvParse(data);
+};
