193ff2f996bbe3c4aab75bcc48d5573aa79691c6
toddnief <todd.nief@gmail.com>
2024-08-12 13:00:41 -0500

    error for small screens

--- dashboard-react/app/page.js
+++ dashboard-react/app/page.js
@@ -24,10 +24,14 @@ const Home = () => {
 
   return (
     <main className="flex flex-col items-start h-[1250px] overflow-hidden">
-      <div className="max-w-[1440px] h-[600px] overflow-hidden mx-auto mb-3">
+      <div className="block lg:hidden p-2">
+        Please use a device that is at least 1280 pixels wide to view the
+        disintegration dashboard.
+      </div>
+      <div className="hidden lg:block max-w-[1280] h-[600px] overflow-hidden mx-auto mb-3">
         <Dashboard />
       </div>
-      <div className="max-w-[1440px] h-[650px] mx-auto">
+      <div className="hidden lg:block max-w-[1280] h-[650px] mx-auto">
         <DashboardControls />
       </div>
     </main>
