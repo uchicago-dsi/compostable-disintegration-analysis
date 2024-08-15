beaaf5f2f8c28396956495aff196ba5bb37a6db6
toddnief <todd.nief@gmail.com>
2024-08-15 15:37:33 -0500

    add info text for checkbox menus

--- dashboard-react/components/DashboardFilterControls.js
+++ dashboard-react/components/DashboardFilterControls.js
@@ -20,24 +20,28 @@ export default function DashboardFilterControls() {
             selectedOptions={snap.filters.selectedMaterialTypes}
             filterKey="selectedMaterialTypes"
             title="Select Generic Material"
+            infoText="Select one or more options to filter the data by the generic material types tested."
           />
           <CheckboxMenu
             options={snap.options["Material Class III"]}
             selectedOptions={snap.filters.selectedSpecificMaterialTypes}
             filterKey="selectedSpecificMaterialTypes"
             title="Select Specific Material"
+            infoText="Select one or more options to filter the data by the specific material types tested."
           />
           <CheckboxMenu
             options={snap.options["Item Format"]}
             selectedOptions={snap.filters.selectedFormats}
             filterKey="selectedFormats"
             title="Select Item Type"
+            infoText="Select one or more options to filter the data by the type of item tested."
           />
           <CheckboxMenu
             options={snap.options["Item Brand"]}
             selectedOptions={snap.filters.selectedBrands}
             filterKey="selectedBrands"
             title="Select Item Brand"
+            infoText="Select one or more options to filter the data by the brands whose items were tested."
           />
         </div>
         <h2 className="text-center"> Operations</h2>
@@ -47,24 +51,28 @@ export default function DashboardFilterControls() {
             selectedOptions={snap.filters.selectedTechnologies}
             filterKey="selectedTechnologies"
             title="Select Technology"
+            infoText="Select one or more options to filter the data by the primary technology used by the facility conducting the field test."
           />
           <CheckboxMenu
             options={Object.keys(temperatureFilterDict)}
             selectedOptions={snap.filters.selectedTemperatureLevels}
             filterKey="selectedTemperatureLevels"
             title="Select Average Temperature"
+            infoText="Select one or more options to filter the data by the average temperature range during the field test."
           />
           <CheckboxMenu
             options={Object.keys(moistureFilterDict)}
             selectedOptions={snap.filters.selectedMoistureLevels}
             filterKey="selectedMoistureLevels"
             title="Select % Moisture"
+            infoText="Select one or more options to filter the data by the average % moisture."
           />
           <CheckboxMenu
             options={Object.keys(trialDurationDict)}
             selectedOptions={snap.filters.selectedTrialDurations}
             filterKey="selectedTrialDurations"
             title="Select Trial Duration"
+            infoText="Select one or more options to filter the data by the duration of the field test."
           />
         </div>
       </div>
